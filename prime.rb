f99329bb5fb62a28955b5926fc6e16eb2e151262
Mostafa Elfishawy <fisho_world@yahoo.com>
2019-01-12 15:33:00 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,13 @@
-def prime?(prime)
- prime= prime.to_i
-if prime % 2 == 0
- prime = true
-else
-prime = false
+nes (12 sloc)  384 Bytes
+def prime?(number)
+  start = 2
+  if number > 1
+    range = (start..number-1).to_a
+    range.none? do |num_to_test| #none of the numbers should return true for none? to return true. If none of the numbers
+                                # return true, then they are not divisible by the number and the number is prime
+      number % num_to_test == 0
+    end
+  else
+    false
+  end
 end
-end
-prime?(4)
